86d3dab09f8d201fc98d48705e94fa7019491f64
Haley Serr <haleykpelletier@gmail.com>
2020-02-21 20:48:31 -0600

    Updates

---
+++ precache-manifest.4ec84a7a8f6af5ee69b80b7da16f03b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b97374c9b6d6032330e3851b94dfb2c",
+    "url": "/gotclick/index.html"
+  },
+  {
+    "revision": "b49eff41dd3d2bfcc0db",
+    "url": "/gotclick/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ff696b4bca202fff6459",
+    "url": "/gotclick/static/js/2.779f4ff8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gotclick/static/js/2.779f4ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b49eff41dd3d2bfcc0db",
+    "url": "/gotclick/static/js/main.421dc1dc.chunk.js"
+  },
+  {
+    "revision": "a8ce503c4e323e29eb55",
+    "url": "/gotclick/static/js/runtime-main.518a792a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gotclick/static/media/logo.5d5d9eef.svg"
+  }
+]);
